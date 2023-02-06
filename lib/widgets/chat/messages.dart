7ee9cf2ba035946cf82a729ad9db5f57eaabe958
mtanaya09 <mtanaya09@gmail.com>
2023-02-06 18:35:08 +0800

    Setup bubble chat widgets

--- lib/widgets/chat/messages.dart
+++ lib/widgets/chat/messages.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sixth_app/widgets/chat/message_bubble.dart';
 
 class Messages extends StatelessWidget {
   @override
@@ -19,9 +20,7 @@ class Messages extends StatelessWidget {
         return ListView.builder(
           reverse: true,
           itemCount: chatDocs.length,
-          itemBuilder: (ctx, index) => Text(
-            chatDocs[index]['text'],
-          ),
+          itemBuilder: (ctx, index) => MessageBubble(chatDocs[index]['text']),
         );
       },
     );
